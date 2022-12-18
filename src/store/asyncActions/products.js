9275719a43c4a20bb2302b0ba7a82fb5eaafab67
Marta <martasagun95@gmail.com>
2022-12-18 16:07:59 +0100

    version without save localStarage products

--- src/store/asyncActions/products.js
+++ src/store/asyncActions/products.js
@@ -2,19 +2,20 @@ import { load_products_action } from '../reducer/productsReducer';
 import { load_products_for_filtration_action } from '../reducer/filterReducer';
 import { load_promotions_action } from '../reducer/promotionsReducer';
 
-const read_local = () => JSON.parse(localStorage.getItem('categoryIdLocal')) ?? '';
-const write_local = (id_cat) => localStorage.setItem('categoryIdLocal', JSON.stringify(id_cat));
+// const read_local = () => JSON.parse(localStorage.getItem('categoryIdLocal')) ?? '';
+// const write_local = (id_cat) => localStorage.setItem('categoryIdLocal', JSON.stringify(id_cat));
 
-const defaultCategoryId = read_local();
+// const defaultCategoryId = read_local();
 
 export const loadProducts = (id) => {
-    write_local(+id);
+    // write_local(+id);
 
     return async dispatch => {
         const response = await fetch('http://localhost:3333/products/all');
         const data = await response.json();
 
-        const payload = data.filter(({ categoryId }) => categoryId === defaultCategoryId);
+        // const payload = data.filter(({ categoryId }) => categoryId === defaultCategoryId);
+        const payload = data.filter(({ categoryId }) => categoryId === +id);
         dispatch(load_products_action(payload));
         dispatch(load_products_for_filtration_action(payload));
         dispatch(load_promotions_action(data));
