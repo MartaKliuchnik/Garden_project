26d235c6cb3e5d7caf31a640459deda148923adc
Marta <martasagun95@gmail.com>
2023-03-16 21:46:47 +0100

    added mobile layout for the main page

--- src/components/Contacts/index.jsx
+++ src/components/Contacts/index.jsx
@@ -5,7 +5,7 @@ import { InstagramOutlined, WhatsAppOutlined } from '@ant-design/icons';
 export default function Contacts() {
     return (
         <div className={['wrapper', s.footer_contact].join(' ')}>
-        <p style={{fontWeight: 700, fontSize: '40px', textShadow: '-1px -1px 0px #3c4139' }}>Available every weekday from 8:00 to 18:00</p>
+        <p>Available every weekday from 8:00 to 18:00</p>
         
         <div className={s.social_network_icons}>
             <a href='https://www.instagram.com/' className={s.instagram_icon}>
